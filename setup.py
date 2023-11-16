2ca542768300bd80d145426fdc8a0f33b96b3d22
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2023-11-16 23:06:54 +0800

    发布 0.1.1 版本

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='bestdori_api',
-    version='0.1.0',
+    version='0.1.1',
     author='WindowsSov8',
     author_email='qwertyuiop2333@hotmail.com',
     description='Bestdori 的各种 API 调用整合，另外附带部分功能',
