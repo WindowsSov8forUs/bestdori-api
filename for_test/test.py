c8df6d9fee5bf9b98d81bad3ba31d110613fbbd9
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2023-11-17 21:00:52 +0800

    Remove test.py

--- for_test/test.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-# 添加父目录的父目录至系统目录中
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from bestdori.post import Post
-proxy = 'http://127.0.0.1:2802'
-print(Post('131271', proxy).get_song())
