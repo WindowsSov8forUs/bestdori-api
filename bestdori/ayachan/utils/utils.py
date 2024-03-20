7b01502084fac91492d6162478a835db10916a32
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2024-03-20 20:54:51 +0800

    update: 添加了 v2 谱面难度分析 API ，并删除了已无效的 v1 API

--- bestdori/ayachan/utils/utils.py
+++ bestdori/ayachan/utils/utils.py
@@ -6,7 +6,12 @@ ayachan 工具库模块'''
 API = {
     'map-info': 'v2/map-info/',
     'bestdori': 'v2/map-info/bestdori/{id}',
-    'DiffAnalysis': 'DiffAnalysis',
+    'version': 'v2/version',
+    'chart_metrics': {
+        'bandori': 'v2/chart/metrics/bandoru/{chart_id}/{diff_str}',
+        'bestdori': 'v2/chart/metrics/bestdori/{chart_id}',
+        'custom': 'v2/chart/metrics/custom/{diff_str}'
+    },
     'levels': {
         'post': 'https://sonolus.ayachan.fun/test/sonolus/levels',
         'get': 'https://sonolus.ayachan.fun/test/sonolus/levels/{uid}/bdv2.json'
