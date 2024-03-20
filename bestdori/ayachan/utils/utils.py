2c3522180eaa75967f2064e2f374a577510b3a84
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2024-03-20 20:43:21 +0800

    update: 添加获取测试服谱面 API 并对测试服 API 结构进行调整

--- bestdori/ayachan/utils/utils.py
+++ bestdori/ayachan/utils/utils.py
@@ -7,6 +7,9 @@ API = {
     'map-info': 'v2/map-info/',
     'bestdori': 'v2/map-info/bestdori/{id}',
     'DiffAnalysis': 'DiffAnalysis',
-    'levels': 'https://sonolus.ayachan.fun/test/sonolus/levels'
+    'levels': {
+        'post': 'https://sonolus.ayachan.fun/test/sonolus/levels',
+        'get': 'https://sonolus.ayachan.fun/test/sonolus/levels/{uid}/bdv2.json'
+    }
 }
 '''ayachan API 集合'''
