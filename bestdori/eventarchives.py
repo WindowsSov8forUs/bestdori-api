87ff5221855b847b5c8589ab4f2d7ee64c8e1639
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2025-03-06 16:26:08 +0800

    feat: 重构 eventarchive 模块

--- bestdori/eventarchives.py
+++ bestdori/eventarchives.py
@@ -3,15 +3,15 @@
 BanG Dream! 活动数据相关操作'''
 from typing import TYPE_CHECKING, Literal, Optional
 
-from httpx import Response
-
 from . import post
+from .user import Me
 from .utils import get_api
 from .utils.network import Api
 from .exceptions import NotExistException
 
 if TYPE_CHECKING:
     from .typing import (
+        Server,
         EventTop,
         PostList,
         EventArchiveAll5,
@@ -21,34 +21,36 @@ if TYPE_CHECKING:
 API = get_api('bestdori.api')
 
 # 获取总活动数据信息
-def get_all(index: Literal[5]=5) -> 'EventArchiveAll5':
+def get_all(index: Literal[5]=5, *, me: Optional[Me] = None) -> 'EventArchiveAll5':
     '''获取总活动信息
 
     参数:
         index (Literal[5], optional): 指定获取哪种 `all.json`
             `5`: 获取所有已有活动数据的简洁信息 `all.5.json`
+        me (Optional[Me], optional): 用户验证信息
 
     返回:
         EventArchiveAll5: 获取到的总活动信息
     '''
-    return Api(API['all']['archives'].format(index=index)).get().json()
+    return Api(API['all']['archives'].format(index=index)).get(
+        cookies=me.__get_cookies__() if me else None,
+    ).json()
 
 # 异步获取总活动数据信息
-async def get_all_async(index: Literal[5]=5) -> 'EventArchiveAll5':
+async def get_all_async(index: Literal[5]=5, *, me: Optional[Me] = None) -> 'EventArchiveAll5':
     '''获取总活动信息
 
     参数:
         index (Literal[5], optional): 指定获取哪种 `all.json`
             `5`: 获取所有已有活动数据的简洁信息 `all.5.json`
+        me (Optional[Me], optional): 用户验证信息
 
     返回:
         EventArchiveAll5: 获取到的总活动信息
     '''
-    response = await Api(API['all']['archives'].format(index=index)).aget()
-    if isinstance(response, Response):
-        return response.json()
-    else:
-        return await response.json()
+    return (await Api(API['all']['archives'].format(index=index)).aget(
+        cookies=await me.__get_cookies_async__() if me else None,
+    )).json()
 
 # 活动数据类
 class EventArchive:
@@ -58,7 +60,7 @@ class EventArchive:
         id (int): 活动 ID
     '''
     # 初始化
-    def __init__(self, id: int) -> None:
+    def __init__(self, id: int, *, me: Optional[Me] = None) -> None:
         '''活动数据类
 
         参数:
@@ -68,6 +70,8 @@ class EventArchive:
         '''活动 ID'''
         self.__info: Optional['EventArchiveInfo'] = None
         '''活动信息'''
+
+        self.__me = me
         return
     
     # 获取活动数据信息
@@ -77,7 +81,7 @@ class EventArchive:
         返回:
             EventArchiveInfo: 活动数据信息
         '''
-        _all = get_all()
+        _all = get_all(me=self.__me)
         if str(self.id) not in _all:
             raise NotExistException(f'Event archive {self.id}')
         self.__info = _all[str(self.id)]
@@ -91,7 +95,7 @@ class EventArchive:
         返回:
             EventArchiveInfo: 活动数据信息
         '''
-        _all = await get_all_async()
+        _all = await get_all_async(me=self.__me)
         if str(self.id) not in _all:
             raise NotExistException(f'Event archive {self.id}')
         self.__info = _all[str(self.id)]
@@ -101,14 +105,14 @@ class EventArchive:
     # 获取排名分数线
     def get_top(
         self,
-        server: Literal[0, 1, 2, 3, 4],
+        server: 'Server',
         mid: Literal['0']='0',
         latest: Literal['1']='1'
     ) -> 'EventTop':
         '''获取排名分数线
 
         参数:
-            server (Literal[0, 1, 2, 3, 4]): 指定服务器
+            server (Server): 指定服务器
                 `0`: 日服
                 `1`: 英服
                 `2`: 台服
@@ -121,25 +125,26 @@ class EventArchive:
             EventTop: 排名分数线数据
         '''
         return Api(API['events']['top']).get(
+            cookies=self.__me.__get_cookies__() if self.__me else None,
             params={
                 'server': server,
                 'event': self.id,
                 'mid': mid,
                 'latest': latest
-            }
+            },
         ).json()
     
     # 异步获取排名分数线
     async def get_top_async(
         self,
-        server: Literal[0, 1, 2, 3, 4],
+        server: 'Server',
         mid: Literal['0']='0',
         latest: Literal['1']='1'
     ) -> 'EventTop':
         '''获取排名分数线
 
         参数:
-            server (Literal[0, 1, 2, 3, 4]): 指定服务器
+            server (Server): 指定服务器
                 `0`: 日服
                 `1`: 英服
                 `2`: 台服
@@ -151,25 +156,22 @@ class EventArchive:
         返回:
             EventTop: 排名分数线数据
         '''
-        response = await Api(API['events']['top']).aget(
+        return (await Api(API['events']['top']).aget(
+            cookies=await self.__me.__get_cookies_async__() if self.__me else None,
             params={
                 'server': server,
                 'event': self.id,
                 'mid': mid,
                 'latest': latest
-            }
-        )
-        if isinstance(response, Response):
-            return response.json()
-        else:
-            return await response.json()
+            },
+        )).json()
 
     # 获取活动数据评论
     def get_comment(
         self,
         limit: int=20,
         offset: int=0,
-        order: Literal['TIME_DESC', 'TIME_ASC']='TIME_ASC'
+        order: Literal['TIME_DESC', 'TIME_ASC']='TIME_ASC',
     ) -> 'PostList':
         '''获取活动数据评论
 
@@ -193,7 +195,8 @@ class EventArchive:
             category_name='EVENTARCHIVE_COMMENT',
             limit=limit,
             offset=offset,
-            order=order
+            order=order,
+            me=self.__me,
         )
     
     # 异步获取活动数据评论
@@ -201,7 +204,7 @@ class EventArchive:
         self,
         limit: int=20,
         offset: int=0,
-        order: Literal['TIME_DESC', 'TIME_ASC']='TIME_ASC'
+        order: Literal['TIME_DESC', 'TIME_ASC']='TIME_ASC',
     ) -> 'PostList':
         '''获取活动数据评论
 
@@ -225,5 +228,6 @@ class EventArchive:
             category_name='EVENTARCHIVE_COMMENT',
             limit=limit,
             offset=offset,
-            order=order
+            order=order,
+            me=self.__me,
         )
