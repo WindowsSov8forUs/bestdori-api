f96e938fb896f15080ef7c93a0706b206c0d2f2b
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2025-03-17 17:02:25 +0800

    feat: 重构 upload 模块

--- bestdori/upload.py
+++ bestdori/upload.py
@@ -1,21 +1,20 @@
 '''`bestdori.upload`
 
 Bestdori 文件相关操作'''
-from typing import Union
 from pathlib import Path
 from hashlib import sha1
 from io import BufferedReader
-
-from httpx import Response
+from typing import Union, Optional
 
 from .user import Me
-from . import settings
-from .utils.utils import API
+from .utils import get_api
 from .utils.network import Api
 from .exceptions import AlreadyUploadedError
 
+API = get_api('bestdori.api')
+
 # 从 Bestdori 获取指定哈希文件字节
-def download(hash: str) -> bytes:
+def download(hash: str, *, me: Optional[Me] = None) -> bytes:
     '''从 Bestdori 获取指定哈希文件字节
 
     参数:
@@ -24,10 +23,12 @@ def download(hash: str) -> bytes:
     返回:
         bytes: 文件字节 `bytes`
     '''
-    return Api(API['upload']['file'].format(hash=hash)).get().content
+    return Api(API['upload']['file'].format(hash=hash)).get(
+        cookies=me.__get_cookies__() if me else None,
+    ).content
 
 # 异步从 Bestdori 获取指定哈希文件字节
-async def adownload(hash: str) -> bytes:
+async def adownload(hash: str, *, me: Optional[Me] = None) -> bytes:
     '''从 Bestdori 获取指定哈希文件字节
 
     参数:
@@ -36,9 +37,9 @@ async def adownload(hash: str) -> bytes:
     返回:
         bytes: 文件字节 `bytes`
     '''
-    response = await Api(API['upload']['file'].format(hash=hash)).aget()
-    if isinstance(response, Response): return response.content
-    return await response.read()
+    return (await Api(API['upload']['file'].format(hash=hash)).aget(
+        cookies=await me.__get_cookies_async__() if me else None,
+    )).content
 
 # 通过哈希值构建 Bestdori 文件 URL
 def hash_to_url(hash: str) -> str:
@@ -62,7 +63,7 @@ class Upload:
         reader (BufferedReader): 文件字节流
     '''
     # 初始化
-    def __init__(self, file_bytes: bytes, name: str, reader: BufferedReader) -> None:
+    def __init__(self, file_bytes: bytes, name: str, reader: BufferedReader, *, me: Me) -> None:
         # 处理文件字节
         hash = sha1(file_bytes).hexdigest() # 计算 SHA-1 哈希
         size = len(file_bytes) # 计算文件大小
@@ -77,11 +78,13 @@ class Upload:
         '''文件名'''
         self._reader: BufferedReader = reader
         '''文件字节流'''
+
+        self.__me = me
         return
     
     # 从路径中获取文件
     @classmethod
-    def from_path(cls, path: Union[str, Path]) -> 'Upload':
+    def from_path(cls, path: Union[str, Path], *, me: Me) -> 'Upload':
         '''从路径中获取文件
 
         参数:
@@ -97,7 +100,7 @@ class Upload:
         file = open(path, 'rb')
         file_bytes = file.read()
         # 返回上传文件对象
-        return cls(file_bytes, file_name, file)
+        return cls(file_bytes, file_name, file, me=me)
     
     # 上传文件
     def upload(self) -> str:
@@ -106,17 +109,8 @@ class Upload:
         返回:
             str: 上传文件的哈希值
         '''
-        try:
-            cookies = settings._cookies()
-        except:
-            if settings._username is None or settings._password is None:
-                raise ValueError('未添加用户信息。')
-            else:
-                settings._me = Me.login(settings._username, settings._password)
-                cookies = settings._cookies()
-        
         if self._reader.closed:
-            raise ValueError('文件流已关闭。')
+            raise ValueError('File stream has been closed.')
         
         # 构建上传负载
         payload = {
@@ -126,7 +120,9 @@ class Upload:
         }
         # 发送预上传请求
         try:
-            Api(API['upload']['prepare']).post(cookies=cookies, data=payload)
+            Api(API['upload']['prepare']).post(
+                cookies=self.__me.__get_cookies__(), data=payload,
+            )
         except AlreadyUploadedError:
             self._reader.close()
             return self._hash
@@ -136,40 +132,33 @@ class Upload:
                 files={
                     'file': (self._name, self._reader)
                 },
-                cookies=cookies
+                cookies=self.__me.__get_cookies__(),
             )
         # 获取文件的哈希值
         hash_get = response.json()['hash']
         #重复查询至多 5 次上传状态
         for _ in range(5):
             # 发送上传状态查询请求
-            response = Api(API['upload']['status'].format(hash=hash_get)).get()
+            response = Api(API['upload']['status'].format(hash=hash_get)).get(
+                cookies=self.__me.__get_cookies__(),
+            )
             # 获取上传状态
             status = response.json()['status']
             # 若上传成功则返回
             if status == 'available':
                 return hash_get
         
-        raise TimeoutError(f'上传文件 {self._name} 超时。')
+        raise TimeoutError(f'Upload file {self._name} timeout.')
     
     # 异步上传文件
-    async def aupload(self) -> str:
+    async def upload_async(self) -> str:
         '''上传文件
 
         返回:
             str: 上传文件的哈希值
         '''
-        try:
-            cookies = settings._cookies()
-        except:
-            if settings._username is None or settings._password is None:
-                raise ValueError('未添加用户信息。')
-            else:
-                settings._me = await Me.alogin(settings._username, settings._password)
-                cookies = settings._cookies()
-        
         if self._reader.closed:
-            raise ValueError('文件流已关闭。')
+            raise ValueError('File stream has been closed.')
         
         # 构建上传负载
         payload = {
@@ -179,7 +168,9 @@ class Upload:
         }
         # 发送预上传请求
         try:
-            await Api(API['upload']['prepare']).apost(cookies=cookies, data=payload)
+            await Api(API['upload']['prepare']).apost(
+                cookies=await self.__me.__get_cookies_async__(), data=payload,
+            )
         except AlreadyUploadedError:
             self._reader.close()
             return self._hash
@@ -189,24 +180,20 @@ class Upload:
                 files={
                     'file': (self._name, self._reader)
                 },
-                cookies=cookies
+                cookies=await self.__me.__get_cookies_async__(),
             )
         # 获取文件的哈希值
-        if isinstance(response, Response):
-            hash_get = response.json()['hash']
-        else:
-            hash_get = (await response.json())['hash']
+        hash_get = response.json()['hash']
         #重复查询至多 5 次上传状态
         for _ in range(5):
             # 发送上传状态查询请求
-            response = await Api(API['upload']['status'].format(hash=hash_get)).aget()
+            response = await Api(API['upload']['status'].format(hash=hash_get)).aget(
+                cookies=await self.__me.__get_cookies_async__(),
+            )
             # 获取上传状态
-            if isinstance(response, Response):
-                status = response.json()['status']
-            else:
-                status = (await response.json())['status']
+            status = response.json()['status']
             # 若上传成功则返回
             if status == 'available':
                 return hash_get
         
-        raise TimeoutError(f'上传文件 {self._name} 超时。')
+        raise TimeoutError(f'Upload file {self._name} timeout.')
