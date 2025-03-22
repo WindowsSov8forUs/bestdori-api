e93a318046c50566cb7b141bcee5b5bbf67fce95
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2025-03-22 22:48:05 +0800

    feat: 更改入口文件导入

--- bestdori/__init__.py
+++ bestdori/__init__.py
@@ -2,94 +2,65 @@
 
 Bestdori 的各种 API 调用整合，另外附带部分功能'''
 
-from .user import User, Me
-
-from .post import Post
-from .charts import Chart
-
-from .characters import Character
-from .cards import Card
-from .costumes import Costume
-from .events import Event
-from .eventarchives import EventArchive
-from .gacha import Gacha
-from .songs import Song
-from .logincampaigns import LoginCampaign
-from .miracleticket import MiracleTicketExchange
-from .comics import Comic
-from .missions import Mission
-
-from .exceptions import (
-    RequestInvalidError,
-    LoginRequiredError,
-    CredentialsInvalidError,
-    UserInvalidError,
-    PostHasNoChartError,
-    PostHasNoSongError,
-    ServerNotAvailableError,
-    AssetsNotExistError,
-)
-
 from . import (
-    user,
-    post,
-    charts,
-    characters,
+    bands,
     cards,
+    characters,
+    charts,
+    comics,
     costumes,
-    events,
     eventarchives,
+    events,
+    eventtop,
+    eventtracker,
+    exceptions,
+    festival,
     gacha,
-    songs,
-    songmeta,
+    icon,
     logincampaigns,
     miracleticket,
-    comics,
     missions,
-    models
+    player,
+    post,
+    skills,
+    songmeta,
+    songs,
+    stamps,
+    thumb,
+    typing,
+    upload,
+    user,
 )
 
 from .settings import settings as settings
 
 __all__ = [
-    'User',
-    'Me',
-    'Post',
-    'Chart',
-    'Character',
-    'Card',
-    'Costume',
-    'Event',
-    'EventArchive',
-    'Gacha',
-    'Song',
-    'LoginCampaign',
-    'MiracleTicketExchange',
-    'Comic',
-    'Mission',
-    'RequestInvalidError',
-    'LoginRequiredError',
-    'CredentialsInvalidError',
-    'UserInvalidError',
-    'PostHasNoChartError',
-    'PostHasNoSongError',
-    'ServerNotAvailableError',
-    'AssetsNotExistError',
-    'user',
-    'post',
-    'charts',
-    'characters',
+    'bands',
     'cards',
+    'characters',
+    'charts',
+    'comics',
     'costumes',
-    'events',
     'eventarchives',
+    'events',
+    'eventtop',
+    'eventtracker',
+    'exceptions',
+    'festival',
     'gacha',
-    'songs',
-    'songmeta',
+    'icon',
     'logincampaigns',
     'miracleticket',
-    'comics',
     'missions',
-    'models',
-    'settings'
+    'player',
+    'post',
+    'skills',
+    'songmeta',
+    'songs',
+    'stamps',
+    'thumb',
+    'typing',
+    'upload',
+    'user',
+    'settings',
 ]
