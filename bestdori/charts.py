ce533f5dd6f66bb1ac91aae48290e52a80f3637f
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2025-10-03 00:27:03 +0800

    feat: 修改 note 与 chart 实现

--- bestdori/charts.py
+++ bestdori/charts.py
@@ -3,8 +3,8 @@
 谱面相关操作'''
 from copy import deepcopy
 from json import dumps, loads
-from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, Dict, List, Optional
+from dataclasses import asdict, dataclass
+from typing import TYPE_CHECKING, Any, Set, Dict, List, Optional, TypedDict
 
 from .utils import get_api
 from .models.note import *
@@ -15,11 +15,48 @@ if TYPE_CHECKING:
     from .typing import DifficultyName
 
 API = get_api('bestdori.api')
-__NOTE_TYPES__ = ['Long', 'Slide', 'BPM', 'Single', 'Directional']
+
+class _BPMDuration(TypedDict):
+    bpm: float
+    duration: float
+
+def _get_note_beat(note: Note) -> float:
+    if isinstance(note, Slide):
+        if note.connections:
+            return note.connections[0].beat
+        return float('inf')
+    return getattr(note, 'beat', float('inf'))
+
+def _handle_bpm_duration(duration: float, prev_bpm: float, bpm_duration_stack: List[_BPMDuration]):
+    while duration > 0.0:
+        if len(bpm_duration_stack) <= 0:
+            bpm_dur = _BPMDuration(bpm=prev_bpm, duration=duration)
+            bpm_duration_stack.append(bpm_dur)
+            break
+        else:
+            top = bpm_duration_stack[-1]
+            if (top['bpm'] > 0.0 and prev_bpm > 0.0) or (top['bpm'] < 0.0 and prev_bpm < 0.0):
+                # 同向 BPM 时长不相减
+                if top['bpm'] == prev_bpm:
+                    top['duration'] += duration
+                else:
+                    bpm_dur = _BPMDuration(bpm=prev_bpm, duration=duration)
+                    bpm_duration_stack.append(bpm_dur)
+                break
+            else:
+                # 反向 BPM 时长相减
+                if top['duration'] >= duration:
+                    top['duration'] -= duration
+                    if top['duration'] <= 0.0:
+                        bpm_duration_stack.pop()
+                    break
+                else:
+                    duration -= top['duration']
+                    bpm_duration_stack.pop()
 
 # 谱面数据类
 @dataclass
-class Statistics:
+class Stats:
     '''谱面数据类
 
     参数:
@@ -33,13 +70,13 @@ class Statistics:
     '''谱面时长'''
     notes: int
     '''谱面音符总数'''
-    bpm: List[float]
+    bpms: List[float]
     '''谱面 BPM 范围'''
     main_bpm: float
     '''谱面主 BPM'''
 
 # 谱面类
-class Chart(List[BasicNote]):
+class Chart(List[Note]):
     '''谱面类，统合针对谱面的一层操作
 
     参数:
@@ -56,18 +93,10 @@ class Chart(List[BasicNote]):
     
     def _construct(self, data: List[Dict[str, Any]]) -> None:
         for note in data:
-            if note['type'] not in __NOTE_TYPES__:
+            NoteType = NOTE_TYPE.get(note.get('type', ''), None)
+            if NoteType is None:
                 continue
-            if note['type'] in ['Long', 'Slide']:
-                self.append(Slide(**note))
-            elif note['type'] == 'BPM':
-                self.append(BPM(**note))
-            elif note['type'] == 'Single':
-                self.append(Single(**note))
-            elif note['type'] == 'Directional':
-                self.append(Directional(**note))
-            else:
-                continue
+            self.append(NoteType(**note))
     
     # 检查是否为 SP 谱面
     @property
@@ -89,167 +118,138 @@ class Chart(List[BasicNote]):
         返回:
             Chart: 处理后谱面
         '''
-        standard_chart = self.copy()
         # 对谱面进行排序
-        standard_chart.sort(key=lambda x: x.beat)
-        # 处理可能出现的 BPM 错位
-        if not isinstance(standard_chart[0], BPM):
-            offset: float = 0.0 # 记录 offset 修正
-            # 第一位不是 BPM，找寻真正的 BPM 线
-            for note in standard_chart:
-                if isinstance(note, BPM):
-                    offset = note.beat
-                    break
-            if offset < 0: # 没有找到 BPM
-                raise ValueError('Unable to find the first BPM note.')
-            # 对谱面节拍进行修正
-            for note in standard_chart:
-                note.move(-offset)
-                if isinstance(note, Slide):
-                    for connection in note.connections:
-                        if connection.beat < 0:
-                            connection.beat = 0
-                        else:
-                            break
-                else:
-                    if note.beat < 0:
-                        note.beat = 0
-                    else:
-                        break
+        self.sort(key=lambda x: _get_note_beat(x))
         
-        # 处理可能出现的不合法滑条节点
-        for note in standard_chart:
-            if not isinstance(note, Slide):
-                continue
-            
-            for connection in note.connections:
-                if connection.next is not None:
-                    if connection.flick:
+        # 偏移量计算
+        offset: float = 0
+        for note in self:
+            if isinstance(note, BPM):
+                offset = note.bpm
+                break
+        
+        # 规范化处理
+        result = Chart([])
+        for note in self:
+            if isinstance(note, Slide):
+                connections = []
+
+                # 统一类型名称
+                note.type = 'Slide'
+                for index, connection in enumerate(note.connections):
+                    # 修正偏移量
+                    connection.beat -= offset
+                    if connection.beat < 0:
+                        connection.beat = 0.0
+                    
+                    # 修正字段值
+                    if index != 0 and index != len(note.connections) - 1:
                         connection.flick = False
-                    if connection.prev is not None:
-                        if connection.skill:
-                            connection.skill = False
+                        connection.skill = False
+                    
+                    connections.append(connection)
+                note.connections = connections
+            else:
+                # 修正偏移量
+                note.beat -= offset
+                if note.beat < 0:
+                    note.beat = 0.0
+            
+            result.append(note)
         
-        # 对谱面节拍进行修正
-        if standard_chart[0].beat != 0:
-            offset = standard_chart[0].beat
-            for note in standard_chart:
-                note.move(-offset)
-        return standard_chart
+        return result
     
-    def __flatten__(self) -> List[BasicNote]:
+    def __flatten__(self) -> List[FlattenedNote]:
         '''将谱面扁平化处理'''
-        flattened_chart = Chart([])
+        notes: List[FlattenedNote] = []
         for note in self:
             if isinstance(note, Slide):
                 for connection in note.connections:
-                    flattened_chart.append(connection)
+                    notes.append(connection)
             else:
-                flattened_chart.append(note)
-        return sorted(flattened_chart, key=lambda x: x.beat)
+                notes.append(note)
+        
+        # 按节拍排序
+        notes.sort(key=lambda x: x.beat)
+
+        # 清理后跟 BPM 音符
+        while notes and isinstance(notes[-1], BPM):
+            notes.pop()
+        
+        return notes
     
     # 谱面数据统计
-    def count(self) -> Statistics:
+    def count(self) -> Stats:
         '''谱面数据统计
 
         返回:
-            Statistics: 统计到的谱面详细数据
+            Stats: 统计到的谱面详细数据
         '''
-        # 初始化统计数据
-        duration: float = 0 # 谱面时长
-        total_notes: int = 0 # 音符总数
-        bpm_list: List[float] = [] # BPM 列表
-        bpm_main: float = 0 # 主 BPM
-        
-        # 临时变量
-        _started: bool = False
-        _bpm_info_list: List[Dict[str, float]] = []
-        _bpm_dict: Dict[float, float] = {}
-        _prev_bpm: float = 0
-        _prev_bpm_beat: float = 0
-        
-        # 谱面扁平化处理
-        _flattened_chart = self.__flatten__()
-        # 遍历谱面
-        for note in _flattened_chart:
-            # 谱面时长计算仅处理一般情况下会遇到的情况
-            # 可能存在但并未考虑到的情况有：
-            # - 首 BPM 为负或中途出现负数时间轴（该情况下只有实际 beat > 0 的谱面才会被渲染，但未被渲染的 note 仍然存在，且可以被上传）
-            # - 最后一个实际 note 出现后出现 BPM 线（该情况下此 BPM 实际并不应该被算作谱面，但并未找到合适的方法来规避掉）
-            # - 负 BPM 与正 BPM 错位交接带来的时长计算问题
-            
-            # BPM 处理
-            if isinstance(note, BPM):
-                if _prev_bpm != 0 and _started: # BPM 不可能为 0
-                    _duration = (note.beat - _prev_bpm_beat) / _prev_bpm * 60
-                    if _prev_bpm > 0:
-                        if _bpm_info_list and _bpm_info_list[-1]['bpm'] == _prev_bpm:
-                            _bpm_info_list[-1]['duration'] += _duration
-                        else:
-                            _bpm_info_list.append({
-                                'bpm': _prev_bpm,
-                                'duration': _duration
-                            })
-                    else:
-                        while _duration < 0:
-                            if _bpm_info_list:
-                                if _bpm_info_list[-1]['duration'] + _duration < 0:
-                                    _duration += _bpm_info_list[-1]['duration']
-                                    _bpm_info_list.pop()
-                                else:
-                                    _bpm_info_list[-1]['duration'] += _duration
-                                    _duration = 0
-                            else:
-                                _duration = 0
-                
-                _prev_bpm = note.bpm
-                _prev_bpm_beat = note.beat
-            
-            else:
-                # 音符处理
-                if not _started:
-                    _prev_bpm_beat = note.beat
-                    _started = True
-                
-                if isinstance(note, Connection) and note.hidden:
-                    # 忽略隐藏滑条节点
-                    continue
-                
-                total_notes += 1
-        
-        if not isinstance(_flattened_chart[-1], BPM):
-            _duration = (_flattened_chart[-1].beat - _prev_bpm_beat) / _prev_bpm * 60
-            if _bpm_info_list and _bpm_info_list[-1]['bpm'] == _prev_bpm:
-                _bpm_info_list[-1]['duration'] += _duration
-            else:
-                _bpm_info_list.append({
-                    'bpm': _prev_bpm,
-                    'duration': _duration
-                })
-        
-        for _bpm_info in _bpm_info_list:
-            if _bpm_info['bpm'] not in bpm_list:
-                bpm_list.append(_bpm_info['bpm'])
-            duration += _bpm_info['duration']
-            if _bpm_dict.get(_bpm_info['bpm']) is None:
-                _bpm_dict[_bpm_info['bpm']] = 0
-            _bpm_dict[_bpm_info['bpm']] += _bpm_info['duration']
-        
-        bpm_main = max(_bpm_dict, key=lambda bpm: _bpm_dict[bpm])
-        
-        return Statistics(
-            duration,
-            total_notes,
-            bpm_list,
-            bpm_main
+        stats = Stats(
+            time=0.0,
+            notes=0,
+            bpms=[],
+            main_bpm=0.0
         )
+
+        # 扁平化谱面
+        notes = self.__flatten__()
+
+        bpm_duration_stack: List[_BPMDuration] = []
+        prev_beat = 0.0
+        prev_bpm = 0.0
+        bpms: Set[float] = set()
+        # 再次遍历音符列表进行统计
+        for note in notes:
+            if isinstance(note, BPM):
+                if prev_bpm != 0.0:
+                    # bpm_duration_stack 为空与 prev_bpm == 0.0 必定同时成立
+                    duration = (note.beat - prev_beat) * 60.0 / abs(prev_bpm)
+                    _handle_bpm_duration(duration, prev_bpm, bpm_duration_stack)
+                
+                prev_bpm = note.bpm
+                prev_beat = note.beat
+                if prev_bpm > 0.0 and prev_bpm not in bpms:
+                    bpms.add(prev_bpm)
+                    stats.bpms.append(prev_bpm)
+            else:
+                if not isinstance(note, Connection) or not note.hidden:
+                    stats.notes += 1
+        
+        # 处理收尾 BPM 时长
+        if prev_bpm != 0.0 and prev_beat != 0.0:
+            duration = (notes[-1].beat - prev_beat) * 60.0 / abs(prev_bpm)
+            _handle_bpm_duration(duration, prev_bpm, bpm_duration_stack)
+            if duration > 0.0 and prev_bpm > 0.0 and prev_bpm not in bpms:
+                bpms.add(prev_bpm)
+                stats.bpms.append(prev_bpm)
+        
+        # 处理 BPM 计算栈计算谱面时长与 BPM 时长统计
+        bpm_time_dict: Dict[float, float] = {}
+        total_time = 0.0
+        for bpm_dur in bpm_duration_stack:
+            total_time += bpm_dur['duration']
+            if bpm_dur['bpm'] in bpm_time_dict:
+                bpm_time_dict[bpm_dur['bpm']] += bpm_dur['duration']
+            else:
+                bpm_time_dict[bpm_dur['bpm']] = bpm_dur['duration']
+        stats.time = total_time
+
+        # 计算主 BPM
+        main_duration = 0.0
+        for bpm, duration in bpm_time_dict.items():
+            if duration > main_duration:
+                main_duration = duration
+                stats.main_bpm = bpm
+        
+        return stats
 
     # 转换为字典列表对象
     def to_list(self) -> List[Dict[str, Any]]:
         '''将 `Chart` 谱面转换为 `List[Dict[str, Any]]` 对象'''
         chart_data: List[Dict[str, Any]] = []
         for note in self:
-            chart_data.append(note.to_dict())
+            chart_data.append(asdict(note))
         return chart_data
     
     # 转换为 json 字符串
@@ -329,38 +329,17 @@ class Chart(List[BasicNote]):
         return cls(response.json()).standardize()
     
     def copy(self) -> 'Chart':
+        """返回当前谱面的深拷贝副本"""
         new_chart = Chart([])
+        new_chart.extend(deepcopy(self))
+        return new_chart
 
-        for note in self:
-            if isinstance(note, Slide):
-                _note = Slide(
-                    type='Slide',
-                    connections=[],
-                )
+    def __copy__(self):
+        new_chart = Chart([])
+        new_chart.extend(self)
+        return new_chart
 
-                prev_connection: Optional[Connection] = None
-                for connection in note:
-                    _connection = Connection(
-                        beat=connection.beat,
-                        lane=connection.lane,
-                        flick=connection.flick,
-                        skill=connection.skill,
-                        hidden=connection.hidden,
-                    )
-                    if prev_connection is not None:
-                        prev_connection.next = _connection
-                        _connection.prev = prev_connection
-                    else:
-                        _note.head = _connection
-                    
-                    if connection.next is None:
-                        _note.tail = _connection
-                    else:
-                        prev_connection = _connection
-                
-                new_chart.append(_note)
-            else:
-                _note = deepcopy(note)
-                new_chart.append(_note)
-        
+    def __deepcopy__(self, memo):
+        new_chart = Chart([])
+        new_chart.extend(deepcopy(list(self), memo))
         return new_chart
--- bestdori/models/note.py
+++ bestdori/models/note.py
@@ -1,309 +1,52 @@
 '''`bestdori.utils.note`
 
 谱面音符相关模块'''
+from dataclasses import dataclass, field
+from typing import Dict, List, Type, Union, Literal, ClassVar, TypeAlias
 
-from typing_extensions import override
-from dataclasses import field, asdict, dataclass
-from typing import Any, Dict, List, Literal, Optional, Generator
-
-@dataclass
-class BasicNote:
-    '''音符基类'''
-    
+@dataclass(kw_only=True)
+class Connection:
+    '''节点音符'''
     beat: float
-    '''音符所在节拍值'''
-    flick: bool
-    '''是否为 flick'''
-    skill: bool
-    '''是否为技能音符'''
-    hidden: bool
-    '''是否为隐藏音符'''
     lane: float
-    '''音符所在轨道'''
+    flick: bool = False
+    skill: bool = False
+    hidden: bool = False
 
-    def to_dict(self) -> Dict[str, Any]:
-        '''字典化'''
-        _dict = {
-            'beat': self.beat,
-            'lane': self.lane,
-        }
-        if self.flick:
-            _dict['flick'] = self.flick
-        if self.skill:
-            _dict['skill'] = self.skill
-        if self.hidden:
-            _dict['hidden'] = self.hidden
-        return _dict
-    
-    def move(self, beat: float) -> None:
-        '''移动音符'''
-        self.beat += beat
+@dataclass(kw_only=True)
+class _Note:
+    type: Literal['Single', 'Slide', 'Long', 'BPM', 'Directional']
 
-@dataclass
-class Note(BasicNote):
-    '''音符类'''
-    type: str
-    '''音符类型'''
-    beat: float = field(default=0, init=False)
-    '''音符所在节拍值'''
-    bpm: Optional[float] = field(default=None, init=False)
-    '''BPM 值'''
-    connections: Optional[List[BasicNote]] = field(default=None, init=False)
-    '''滑条节点音符列表'''
-    direction: Optional[Literal['Left', 'Right']] = field(default=None, init=False)
-    '''滑键方向'''
-    flick: Optional[bool] = field(default=None, init=False)
-    '''是否为 flick'''
-    skill: Optional[bool] = field(default=None, init=False)
-    '''是否为技能音符'''
-    hidden: Optional[bool] = field(default=None, init=False)
-    '''是否为隐藏音符'''
-    lane: Optional[float] = field(default=None, init=False)
-    '''音符所在轨道'''
-    width: Optional[Literal[1, 2, 3]] = field(default=None, init=False)
-    '''滑键宽度'''
+@dataclass(kw_only=True)
+class Single(_Note):
+    beat: float
+    lane: float
+    flick: bool = False
+    skill: bool = False
+    hidden: bool = False
 
-    @override
-    def to_dict(self) -> Dict[str, Any]:
-        '''字典化'''
-        return asdict(self)
+@dataclass(kw_only=True)
+class Slide(_Note):
+    connections: List[Connection] = field(default_factory=list)
 
-@dataclass
-class BPM(Note):
-    '''BPM 音符'''
-    type: Literal['BPM'] = field(default='BPM')
-    '''音符类型'''
-    bpm: float = field(default=120)
-    '''BPM 值'''
-    beat: float = field(default=0)
-    '''音符所在节拍值'''
+@dataclass(kw_only=True)
+class BPM(_Note):
+    beat: float
+    bpm: float
 
-    @override
-    def __init__(
-        self,
-        *,
-        type: Literal['BPM'],
-        bpm: float,
-        beat: float,
-        **kwargs: Any,
-    ) -> None:
-        super().__init__(
-            type=type,
-        )
+@dataclass(kw_only=True)
+class Directional(_Note):
+    beat: float
+    lane: float
+    direction: Literal['Left', 'Right']
 
-        self.bpm = bpm
-        self.beat = beat
+Note: TypeAlias = Union[Single, Slide, BPM, Directional]
+FlattenedNote: TypeAlias = Union[Single, Connection, BPM, Directional]
 
-@dataclass
-class Single(Note):
-    '''单点音符'''
-    type: Literal['Single'] = field(default='Single')
-    '''音符类型'''
-    beat: float = field(default=0)
-    '''音符所在节拍值'''
-    lane: float = field(default=0)
-    '''音符所在轨道'''
-    flick: bool = field(default=False)
-    '''是否为 flick'''
-    skill: bool = field(default=False)
-    '''是否为技能音符'''
-
-    @override
-    def __init__(
-        self,
-        *,
-        type: Literal['Single'],
-        beat: float,
-        lane: float,
-        flick: bool = False,
-        skill: bool = False,
-        **kwargs: Any,
-    ) -> None:
-        super().__init__(
-            type=type,
-        )
-        
-        self.beat = beat
-        self.lane = lane
-        self.flick = flick
-        self.skill = skill
-
-@dataclass
-class Directional(Note):
-    '''方向滑键音符'''
-    type: Literal['Directional'] = field(default='Directional')
-    '''音符类型'''
-    beat: float = field(default=0)
-    '''音符所在节拍值'''
-    lane: float = field(default=0)
-    '''音符所在轨道'''
-    width: Literal[1, 2, 3] = field(default=1)
-    '''滑键宽度'''
-    direction: Literal['Left', 'Right'] = field(default='Left')
-    '''滑键方向'''
-
-    @override
-    def __init__(
-        self,
-        *,
-        type: Literal['Directional'],
-        beat: float,
-        lane: float,
-        width: Literal[1, 2, 3],
-        direction: Literal['Left', 'Right'],
-        **kwargs: Any,
-    ) -> None:
-        super().__init__(
-            type=type,
-        )
-
-        self.beat = beat
-        self.lane = lane
-        self.width = width
-        self.direction = direction
-
-@dataclass
-class Connection(BasicNote):
-    '''滑条节点音符'''
-    prev: Optional['Connection'] = field(default=None)
-    '''上一个滑条节点音符'''
-    next: Optional['Connection'] = field(default=None)
-    '''下一个滑条节点音符'''
-
-    @override
-    def __init__(
-        self,
-        *,
-        beat: float,
-        lane: float,
-        flick: bool = False,
-        skill: bool = False,
-        hidden: bool = False,
-        **kwargs: Any,
-    ) -> None:
-        self.beat = beat
-        self.lane = lane
-        self.flick = flick
-        self.skill = skill
-        self.hidden = hidden
-    
-    @override
-    def __eq__(self, other: 'Connection') -> bool:
-        '''判断是否相等'''
-        if self is other:
-            return True
-        else:
-            return (
-                self.beat == other.beat and
-                self.lane == other.lane and
-                self.flick == other.flick and
-                self.skill == other.skill and
-                self.hidden == other.hidden and
-                self.prev == other.prev and
-                self.next == other.next
-            )
-
-@dataclass
-class Slide(Note):
-    '''滑条音符'''
-    type: Literal['Slide'] = field(default='Slide')
-    '''音符类型'''
-    head: Optional[Connection] = field(default=None, init=False)
-    '''滑条头节点'''
-    tail: Optional[Connection] = field(default=None, init=False)
-    '''滑条尾节点'''
-
-    @override
-    def __init__(
-        self,
-        *,
-        type: Literal['Slide', 'Long'],
-        connections: List[Dict[str, Any]],
-        **kwargs: Any,
-    ) -> None:
-        type = 'Slide' if type == 'Long' else type
-        super().__init__(
-            type=type,
-        )
-
-        for connection in connections:
-            self.append(**connection)
-    
-    def __iter__(self) -> Generator[Connection, None, None]:
-        '''迭代器'''
-        current = self.head
-        while current:
-            yield current
-            current = current.next
-    
-    @property
-    def connections(self) -> List[Connection]:
-        '''滑条节点音符列表'''
-        return list(self)
-
-    @property
-    def beat(self) -> float:
-        '''音符所在节拍值'''
-        return self.head.beat if self.head else 0
-
-    @beat.setter
-    def beat(self, value: float) -> None:
-        '''设置音符所在节拍值'''
-        move = value - self.beat
-        self.move(move)
-
-    def append(self, **kwargs: Any) -> None:
-        '''添加滑条节点音符到尾部'''
-        connection = Connection(**kwargs)
-
-        if not self.head or not self.tail:
-            self.head = connection
-            self.tail = connection
-        else:
-            self.tail.next = connection
-            connection.prev = self.tail
-            self.tail = connection
-    
-    def prepend(self, **kwargs: Any) -> None:
-        '''添加滑条节点音符到头部'''
-        connection = Connection(**kwargs)
-        connection.next = self.head
-        self.head = connection
-    
-    def delete(self, connection: Connection) -> None:
-        '''删除滑条节点音符'''
-        if not self.head:
-            return
-        
-        if self.head == connection:
-            self.head = self.head.next
-            return
-        
-        current = self.head
-        while current.next and current.next != connection:
-            current = current.next
-        
-        if current.next:
-            current.next = current.next.next
-
-    def to_dict(self) -> Dict[str, Any]:
-        '''字典化'''
-        return {
-            'type': self.type,
-            'connections': [connection.to_dict() for connection in self],
-        }
-    
-    def move(self, beat: float) -> None:
-        '''移动音符'''
-        for connection in self:
-            connection.move(beat)
-
-__all__ = [
-    'BasicNote',
-    'Note',
-    'BPM',
-    'Single',
-    'Directional',
-    'Connection',
-    'Slide',
-]
+NOTE_TYPE: Dict[str, Type[Note]] = {
+    'Single': Single,
+    'Slide': Slide,
+    'Long': Slide,
+    'BPM': BPM,
+    'Directional': Directional,
+}
